77adad8d1315be6bbe1307d36b50e17a5f25565a
Mário César Silva <mcgato33@hotmail.com>
2020-08-29 21:47:40 -0100

    continuacao

---
+++ src/components/ChannelList/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const List =  styled.View``;
+
+export const ChannelContainer = styled.View``;
+
+export const LeftSide = styled.View``;
+
+export const Avatar = styled.View``;
+
+export const Column = styled.View``;
+
+export const UserName = styled.View``;
+
+export const Info = styled.View``;
+
+export const RightSide = styled.View``;
+
+export const WhiteCircle = styled.View``;
